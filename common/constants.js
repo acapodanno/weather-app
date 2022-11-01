9b85fd4d3c449ce9332081aaad1ae67a1351b906
Alessandro Capodanno <alessandro.capodanno95@gmail.com>
2022-11-01 23:00:18 +0100

    add all

--- common/constants.js
+++ common/constants.js
@@ -1,4 +1,4 @@
-const APP_ID = '04f18d8210cf1f25104c849d7484db5d'
+const APP_ID = ''
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const STATUS_CODE = require('http').STATUS_CODES
 
